29cf0563c1760714976f9948f4f844767e2e723b
yanxingzhe <yanxingzhe@pinduoduo.com>
2019-02-14 00:14:13 +0800

    update official demo

--- src/official/Demo.js
+++ src/official/Demo.js
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
 
+function FC(props) {
+  const [count, setCount] = useState(0);
+  console.log("MemoedFC render", props);
+  return (
+    <div>
+      count: {count}
+      <button onClick={() => setCount(count + 1)}>add Count</button>
+    </div>
+  );
+}
+
+const MemoedFC = React.memo(FC);
+
 const Demo = () => {
   const [name, setName] = useState("noName");
   const [address, setAddress] = useState("noAddrress");
@@ -14,6 +27,7 @@ const Demo = () => {
     setAddress(val);
     console.log(val);
   };
+  console.log("Demo render");
 
   return (
     <div>
@@ -21,6 +35,7 @@ const Demo = () => {
       name: {name} <input name={name} onInput={handleNameChange} />
       <br />
       address: {address} <input name={address} onInput={handleAddressChange} />
+      <MemoedFC name={name} />
     </div>
   );
 };
